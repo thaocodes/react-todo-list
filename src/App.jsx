38ae63279191a23d1a6779f93ba5f5b9c0825f4d
thao <thaole0325@gmail.com>
2023-09-08 08:58:01 -0400

    adding items to todo list displays each item with checkbox & delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  
+    // pass function to `setTodos` to add new items to `todos` array
+    setTodos((currentTodos) => {
+      // function returns whatever value we want state to be
+      // takes one arg `currentTodos` which is current value of our state 
+      return [
+        ...currentTodos, // spread operator copies `currentTodos` array & adds new item: 
+        { id: crypto.randomUUID(), title: newItem, completed: false }, // new item
+      ]
+    })
+    // clear out input box after each `todo` item gets added
+    setNewItem("") 
+  }
+  
+
   return (
   // wrap in fragment, returns more than 1 element
   <> 
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
-        {/* need to link up label on our input, use htmlFor (instead if for) bc JSX */}
+        {/* need to link up label to input, use `htmlFor` (instead of `for`) for JSX */}
         <label htmlFor="Item">New Item</label>
-        <input type="text" id="item" />
+
+        {/* update input when it changes */}
+        <input 
+          value={newItem} // set the value of input to whatever our newItem variable is 
+          onChange={e => setNewItem(e.target.value)}
+          type="text" 
+          id="item" />
       </div>
       <button className="btn">Add</button>
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <i>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </i>
-      <i>
-        <label>
-          <input type="checkbox" />
-          Item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </i>
+      {/* for each todo, we want to return one <li> element */}
+      {todos.map(todo => { // map returns an array 
+      // need to add key prop when returning an array of elements
+      // add this to the very top level <li>
+        return (
+          // unique identifier for each todo element 
+          <li key={todo.id}> 
+            <label>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
     </ul>
   </>
   )
